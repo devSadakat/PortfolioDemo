4bcd69edfd6c9eb278612b34c1b180cd42964be7
sadakat hossain <sadakat.sh1@gmail.com>
2024-12-26 10:44:46 +0700

    Setup Rotere for this portfolio website

---
+++ src/Layoute/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MainLayout = () => {
+    return (
+        <div>
+            <h2>This is the main Layout page of Portfolio Assignment</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
+import {
+  RouterProvider,
+} from "react-router";
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { router } from "./Routes/Routes";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
