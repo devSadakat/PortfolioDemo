7c97fba944cee26a92fb1f2ac8584f2180eadb3d
sadakat hossain <sadakat.sh1@gmail.com>
2025-01-10 01:26:43 +0700

    Setup A plan and pre design

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,26 @@
 const Home = () => {
     return (
         <div>
-            <h1> his is Home page of this Portfolio website. </h1>
+            <h1>Nav Bar wiht profile photo</h1>
+            {/* Div 1 */}
+            <div>
+                {/* This need to be make flex */}
+                <div>
+                    <h1>Proffession</h1>
+                    <p>Short Descriptions</p>
+                </div>
+                <div>
+                    <img src="Profile Photo" alt="" />
+                </div>
+            </div>
+            {/* Div 2 */}
+            <div>Experties</div>
+            {/* Div 3 */}
+            <div>Projects</div>
+            {/* Div 4 */}
+            <div>Career Experience</div>
+            {/* Div 5 */}
+            <div>Contact Section</div>
         </div>
     );
 };
